08519094b6d9697b36974cc00a789f65a2e7def8
deveq <deveq@kakao.com>
2021-12-13 19:00:08 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+﻿const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function(e) {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus',function(e) {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+searchInputEl.addEventListener('blur', () => {
+    searchInputEl.removeAttribute('placeholder');
+    searchEl.classList.remove('focused');
+})
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle((e)=> {
+    console.log(window.scrollY);
+},300));
